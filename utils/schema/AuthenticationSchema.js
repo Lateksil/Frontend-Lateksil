18adc4b6ec21308f767364658d7bae9d0fc8a964
Devaaji <devaaji5@gmail.com>
2023-04-13 20:26:31 +0700

    [ADD] - resolver schema register

--- utils/schema/AuthenticationSchema.js
+++ utils/schema/AuthenticationSchema.js
@@ -1,5 +1,18 @@
 import * as Yup from "yup";
 
+export const registerSchema = Yup.object().shape({
+  full_name: Yup.string().required("Nama Lengkap harus di isi"),
+  company_name: Yup.string().required("Nama Perusahaan harus di isi"),
+  address: Yup.string().required("Alamat harus di isi"),
+  no_whatsapp: Yup.string().required("Nama Perusahaan harus di isi"),
+  email: Yup.string()
+    .email("Format Email tidak sesuai")
+    .required("Email harus diisi"),
+  password: Yup.string()
+    .required("Password Harus diisi")
+    .min(6, "Minimal terdiri dari 6 karakter"),
+});
+
 export const loginSchema = Yup.object().shape({
   email: Yup.string()
     .email("Format Email tidak sesuai")
