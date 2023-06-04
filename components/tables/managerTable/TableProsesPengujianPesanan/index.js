66b0d615112ab1aca1690f608c1632323af4b58f
Devaaji <devaaji5@gmail.com>
2023-06-04 19:18:25 +0700

    [ADD] detail proses pengujian

--- components/tables/managerTable/TableProsesPengujianPesanan/index.js
+++ components/tables/managerTable/TableProsesPengujianPesanan/index.js
@@ -18,8 +18,15 @@ import { FaTools, FaUserPlus } from 'react-icons/fa';
 import PilihTeknisiModal from '../../../modals/managerModal/pilihTeknisiModal';
 import ParseDate from '../../../core/parseDate';
 import AddPeralatanModal from '../../../modals/managerModal/addPeralatanModal';
+import DetailProsesPengujian from '../../../modals/managerModal/detailProsesPengujian';
 
 const TableProsesPengujianPesanan = ({ pengujian }) => {
+  const {
+    isOpen: isOpenDetailPengujian,
+    onOpen: onOpenDetailPengujian,
+    onClose: onCloseDetailPengujian,
+  } = useDisclosure();
+
   const {
     isOpen: isOpenAddPeralatan,
     onOpen: onOpenAddPeralatan,
@@ -35,22 +42,33 @@ const TableProsesPengujianPesanan = ({ pengujian }) => {
   return (
     <>
       <Tr cursor="pointer">
-        <Td>
+        <Td onClick={onOpenDetailPengujian}>
           <Flex direction="column" w="full">
             <Text fontWeight="semibold">{pengujian.User.full_name}</Text>
             <Text>{pengujian.User.company_name}</Text>
           </Flex>
         </Td>
-        <Td textAlign="center">{pengujian.proyek.nama_proyek}</Td>
-        <Td textAlign="center" color="blue.700" fontWeight="semibold">
+        <Td textAlign="center" onClick={onOpenDetailPengujian}>
+          {pengujian.proyek.nama_proyek}
+        </Td>
+        <Td
+          textAlign="center"
+          color="blue.700"
+          fontWeight="semibold"
+          onClick={onOpenDetailPengujian}
+        >
           Rp{formatCurrency(pengujian.total_price)}
         </Td>
-        <Td textAlign="center">{ParseDate(pengujian.createdAt)}</Td>
-        <Td textAlign="center">{ParseDate(pengujian.proyek.tanggal_mulai)}</Td>
-        <Td textAlign="center">
+        <Td textAlign="center" onClick={onOpenDetailPengujian}>
+          {ParseDate(pengujian.createdAt)}
+        </Td>
+        <Td textAlign="center" onClick={onOpenDetailPengujian}>
+          {ParseDate(pengujian.proyek.tanggal_mulai)}
+        </Td>
+        <Td textAlign="center" onClick={onOpenDetailPengujian}>
           {ParseDate(pengujian.proyek.tanggal_selesai)}
         </Td>
-        <Td textAlign="center">
+        <Td textAlign="center" onClick={onOpenDetailPengujian}>
           <Badge colorScheme="green" px="4" py="2">
             Lunas
           </Badge>
@@ -81,10 +99,24 @@ const TableProsesPengujianPesanan = ({ pengujian }) => {
               >
                 Peralatan
               </MenuItem>
+              <MenuItem
+                icon={<FaTools size={15} />}
+                color="orange.600"
+                fontWeight="semibold"
+                isDisabled={true}
+                onClick={onOpenAddPeralatan}
+              >
+                Kirim
+              </MenuItem>
             </MenuList>
           </Menu>
         </Td>
       </Tr>
+      <DetailProsesPengujian
+        id={pengujian.id}
+        isOpen={isOpenDetailPengujian}
+        onClose={onCloseDetailPengujian}
+      />
       <AddPeralatanModal
         id={pengujian.id}
         isOpen={isOpenAddPeralatan}
