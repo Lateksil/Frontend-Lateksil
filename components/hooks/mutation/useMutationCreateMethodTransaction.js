8d7baa359583bccf3716fd92d5b953ce4ed3b5ba
Devaaji <devaaji5@gmail.com>
2023-07-07 19:25:33 +0700

    [POST] in method transaction create

---
+++ components/hooks/mutation/useMutationCreateMethodTransaction.js
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { postFetcher } from '../../../libs/axios';
+import useToastNotification from '../useToastNotification';
+
+const useMutationCreateMethodTransaction = () => {
+  const queryClient = useQueryClient();
+  const showToast = useToastNotification();
+
+  const uri = '/method_transaction/create';
+
+  const { mutateAsync, ...others } = useMutation(
+    async (formData) => {
+      const data = await postFetcher(uri, formData);
+      return data;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('method_transaction').then(() => {
+          showToast('Berhasil Menambahkan', 'success');
+        });
+      },
+    }
+  );
+
+  return { mutateAsync, ...others };
+};
+
+export default useMutationCreateMethodTransaction;
